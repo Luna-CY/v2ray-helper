0c959a7a6ad69594625aae265322362c3855fce6
Huo Rong Hui <772047518@qq.com>
2021-09-16 23:08:51 +0800

    提交DNS解析

--- common/util/net.go
+++ common/util/net.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"io/ioutil"
+	"net"
 	"net/http"
 	"os/exec"
 )
@@ -24,6 +25,28 @@ func GetPublicIpv4() (string, error) {
 	return string(ipBytes), nil
 }
 
+// CheckHostIsAllow 检查HOST是否已解析到当前服务器
+func CheckHostIsAllow(host string) (bool, error) {
+	publicIp, err := GetPublicIpv4()
+	if nil != err {
+		return false, err
+	}
+
+	ipList, err := net.LookupHost(host)
+	if nil != err {
+		return false, errors.New(fmt.Sprintf("解析DNS失败: %v", err))
+	}
+
+	for _, ip := range ipList {
+		if ip == publicIp {
+			return true, nil
+		}
+	}
+
+	return false, nil
+}
+
+// CheckLocalPortIsAllow 检查本地端口是否可用
 func CheckLocalPortIsAllow(port int) (bool, error) {
 	res, err := exec.Command("lsof", "-i", fmt.Sprintf(":%v", port)).Output()
 	if nil != err && 0 != len(res) {
